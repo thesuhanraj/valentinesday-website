48bbf010ec1b38f02ad729d7dee364afe193489d
thesuhanraj <suhanraj3@gmail.com>
2025-02-14 12:23:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const heart = document.getElementById("heart");
     const messageBox = document.getElementById("love-letter");
     const closeButton = document.getElementById("closeBtn");
-
+    
     heart.addEventListener("click", function () {
         this.classList.add("separate-heart");
 
         setTimeout(() => {
             document.body.classList.add("neon-bg");
             document.getElementById("message").classList.add("reveal-text");
-
             messageBox.style.display = "block";
-            messageBox.style.position = "fixed";
-            messageBox.style.bottom = "20px";
-            messageBox.style.left = "20px";
         }, 1000);
     });
 
     closeButton.addEventListener("click", function () {
         messageBox.style.display = "none";
     });
+
+    messageBox.style.position = "fixed";
+    messageBox.style.bottom = "20px";
+    messageBox.style.left = "20px";
 
     document.body.style.overflow = "hidden";
     document.documentElement.style.overflow = "hidden";
